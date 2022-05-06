a82588043c0d6ce2753d11e0a50f4300bf2df3b3
InitialShip <motosawachii@gmail.com>
2022-05-06 16:45:35 +0700

    api call test

--- app/src/main/java/com/example/englishdictionary/fragment/SearchFragment.java
+++ app/src/main/java/com/example/englishdictionary/fragment/SearchFragment.java
@@ -7,8 +7,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.example.englishdictionary.MainActivity;
 import com.example.englishdictionary.R;
